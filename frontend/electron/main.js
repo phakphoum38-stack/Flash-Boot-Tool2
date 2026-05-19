2233deb928d521c5c17219aafc5c022b2182a8e9
phakphoum38-stack <phakphoum38@gmail.com>
2026-05-19 11:09:00 +0700

    Update main.js

--- frontend/electron/main.js
+++ frontend/electron/main.js
@@ -4,10 +4,8 @@ const path = require("path");
 
 let backend;
 
-// pfunction startBackend() {
-
+function startBackend() {
   let exePath;
-
   if (app.isPackaged) {
     exePath = path.join(process.resourcesPath, "backend", "backend.exe");
   } else {
@@ -15,24 +13,17 @@ let backend;
   }
 
   console.log("Backend:", exePath);
-
-  backend = spawn(exePath, [], {
-    windowsHide: true
-  });
+  backend = spawn(exePath, [], { windowsHide: true });
 
   backend.stdout.on("data", d => {
     console.log(d.toString());
   });
-
   backend.stderr.on("data", d => {
     console.error(d.toString());
   });
 }
 
-// p
-% WINDOW
 function createWindow() {
-
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
@@ -41,43 +32,26 @@ function createWindow() {
       contextIsolation: true
     }
   });
-
   win.loadFile(path.join(__dirname, "../dist/index.html"));
 }
 
-// pipcMain.handle("select-iso", async () => {
-
+ipcMain.handle("select-iso", async () => {
   const result = await dialog.showOpenDialog({
-    filters: [
-      {
-        name: "ISO",
-        extensions: ["iso"]
-      }
-    ],
+    filters: [{ name: "ISO", extensions: ["iso"] }],
     properties: ["openFile"]
   });
-
   if (result.canceled) return null;
-
   return result.filePaths[0];
 });
 
-// papp.whenReady().then(() => {
-
+app.whenReady().then(() => {
   startBackend();
-
   setTimeout(() => {
     createWindow();
   }, 2000);
 });
 
-// papp.on("will-quit", () => {
+app.on("will-quit", () => {
   if (backend) backend.kill();
 });
-const { dialog } = require('electron');
-ipcMain.handle('select-iso-file', async () => {
-  const { canceled, filePaths } = await dialog.showOpenDialog({
-    filters: [{ name: 'ISO', extensions: ['iso'] }]
-  });
-  return canceled? null : filePaths[0];
 });
