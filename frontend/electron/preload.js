47dc3cf28fe9e9e2520b71f2fb949eb6ab3de286
phakphoum38-stack <phakphoum38@gmail.com>
2026-05-19 09:37:32 +0700

    Update preload.js

--- frontend/electron/preload.js
+++ frontend/electron/preload.js
@@ -1,7 +1,8 @@
-const { contextBridge, ipcRenderer } = require("electron");
+const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld("api", {
-
-  selectISO: () => ipcRenderer.invoke("select-iso")
-
+contextBridge.exposeInMainWorld('electron', {
+  flashIso: (isoPath, device) => ipcRenderer.invoke('flash-iso', isoPath, device),
+  onFlashProgress: (callback) => ipcRenderer.on('flash-progress', (_, progress) => callback(progress)),
+  onFlashError: (callback) => ipcRenderer.on('flash-error', (_, error) => callback(error)),
+  removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel)
 });
