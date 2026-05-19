cb4a452e66f9b8f082c49b2dec35a2a63e8cabc5
phakphoum38-stack <phakphoum38@gmail.com>
2026-05-19 09:42:12 +0700

    Update preload.js

--- frontend/electron/preload.js
+++ frontend/electron/preload.js
@@ -5,4 +5,5 @@ contextBridge.exposeInMainWorld('electron', {
   onFlashProgress: (callback) => ipcRenderer.on('flash-progress', (_, progress) => callback(progress)),
   onFlashError: (callback) => ipcRenderer.on('flash-error', (_, error) => callback(error)),
   removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel)
+  selectIsoFile: () => ipcRenderer.invoke('select-iso-file')
 });
