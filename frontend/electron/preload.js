43a3a37e80a68c8c9e1b9b5746e27518f18c3606
phakphoum38-stack <phakphoum38@gmail.com>
2026-05-20 01:58:47 +0700

    Update preload.js

--- frontend/electron/preload.js
+++ frontend/electron/preload.js
@@ -3,14 +3,13 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('electron', {
   flashIso: (isoPath, device) => ipcRenderer.invoke('flash-iso', isoPath, device),
   
-  onFlashProgress: (callback) => 
-    ipcRenderer.on('flash-progress', (_, progress) => callback(progress)),
+  onFlashProgress: (callback) => ipcRenderer.on('flash-progress', (_, progress) => callback(progress)),
   
-  onFlashError: (callback) => 
-    ipcRenderer.on('flash-error', (_, error) => callback(error)),
+  onFlashError: (callback) => ipcRenderer.on('flash-error', (_, error) => callback(error)),
   
   removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel),
-
-  selectIsoFile: () => ipcRenderer.invoke('select-iso')
-  getUsbDevices: () => ipcRenderer.invoke('get-usb-devices')
+  
+  selectIsoFile: () => ipcRenderer.invoke('select-iso'),
+  
+  getUsbDevices: () => ipcRenderer.invoke('get-usb-devices') // ต้องมี , ข้างบนด้วย
 })
