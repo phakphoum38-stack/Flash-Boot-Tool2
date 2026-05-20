16efeafb188db4f7825f034710680b55d6a4eee0
phakphoum38-stack <phakphoum38@gmail.com>
2026-05-20 08:06:03 +0700

    Create preload.ts

---
+++ frontend/electron/preload.ts
@@ -0,0 +1,21 @@
+import { contextBridge, ipcRenderer } from 'electron'
+
+type FlashEvent = 
+  | { type: 'progress', value: number, written: number, total: number }
+  | { type: 'log', level: string, msg: string }
+  | { type: 'result', success: boolean, msg?: string }
+  | { type: 'error', msg: string }
+
+contextBridge.exposeInMainWorld('electron', {
+  flashIso: (mode: string, isoPath: string, device: string) => 
+    ipcRenderer.invoke('flash-iso', mode, isoPath, device),
+  
+  onFlashEvent: (callback: (event: FlashEvent) => void) => {
+    const handler = (_: any, event: FlashEvent) => callback(event)
+    ipcRenderer.on('flash-event', handler)
+    return () => ipcRenderer.removeListener('flash-event', handler)
+  },
+  
+  selectIsoFile: () => ipcRenderer.invoke('select-iso'),
+  getUsbDevices: () => ipcRenderer.invoke('get-usb-devices')
+})
