89979b26de9412fc988a06ab8d28bca2321da1fa
phakphoum38-stack <phakphoum38@gmail.com>
2026-05-25 15:40:12 +0700

    Update preload.js

--- frontend/electron/preload.js
+++ frontend/electron/preload.js
@@ -1,23 +1,13 @@
 console.log('preload loaded')
 const { contextBridge, ipcRenderer } = require('electron')
 
-contextBridge.exposeInMainWorld('electron', {
-  flashIso: (isoPath, device) => ipcRenderer.invoke('flash-iso', isoPath, device),
-  
-  onFlashProgress: (callback) => 
-    ipcRenderer.on('flash-progress', (_, progress) => callback(progress)),
-  
-  onFlashError: (callback) => 
-    ipcRenderer.on('flash-error', (_, error) => callback(error)),
-  
-  removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel),
-  
-  selectIsoFile: () => ipcRenderer.invoke('select-iso'),
-  
+contextBridge.exposeInMainWorld('electronAPI', {
+  selectIso: () => ipcRenderer.invoke('select-iso'),
   getUsbDevices: () => ipcRenderer.invoke('get-usb-devices'),
-  
-  // เพิ่ม 3 ตัวนี้ให้ตรงกับปุ่ม Pause/Resume/Cancel
-  cancelFlash: () => ipcRenderer.send('cancel-flash'),
+  flashIso: (mode, isoPath, device) => ipcRenderer.invoke('flash-iso', mode, isoPath, device),
+  onFlashEvent: (callback) => ipcRenderer.on('flash-event', (_, data) => callback(data)),
   pauseFlash: () => ipcRenderer.send('pause-flash'),
-  resumeFlash: () => ipcRenderer.send('resume-flash')
+  resumeFlash: () => ipcRenderer.send('resume-flash'),
+  cancelFlash: () => ipcRenderer.send('cancel-flash'),
+  removeAllListeners: (channel) => ipcRenderer.removeAllListeners(channel)
 })
