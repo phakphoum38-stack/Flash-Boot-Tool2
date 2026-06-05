8c5bd55c3591359737bbb2ee47f498ca131ee632
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-05 21:10:44 +0700

    Update preload.js

--- frontend/electron/preload.js
+++ frontend/electron/preload.js
@@ -1,23 +1,19 @@
 const { contextBridge, ipcRenderer } = require("electron")
 
 contextBridge.exposeInMainWorld("electronAPI", {
+
   selectIso: () => ipcRenderer.invoke("select-iso"),
 
   getUsbDevices: () => ipcRenderer.invoke("get-usb-devices"),
 
-  flashIso: (mode, isoPath, device) =>
-    ipcRenderer.invoke("flash-iso", mode, isoPath, device),
+  flashIso: (mode, iso, device) =>
+    ipcRenderer.invoke("flash-iso", mode, iso, device),
 
   cancelFlash: () => ipcRenderer.send("cancel-flash"),
-
-  pauseFlash: () => ipcRenderer.send("pause-flash"),
-
-  resumeFlash: () => ipcRenderer.send("resume-flash"),
 
   onFlashEvent: (cb) => {
     const handler = (_, data) => cb(data)
     ipcRenderer.on("flash-event", handler)
-
     return () => ipcRenderer.removeListener("flash-event", handler)
   }
 })
