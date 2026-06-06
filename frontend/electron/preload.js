510c4a93830b9f9bdabf5cf46715ab014531b895
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-06 11:36:09 +0700

    Update preload.js

--- frontend/electron/preload.js
+++ frontend/electron/preload.js
@@ -1,30 +1,44 @@
 const { contextBridge, ipcRenderer } = require("electron")
 
-let listeners = new Set()
-
 contextBridge.exposeInMainWorld("electron", {
+
   flashIso: (mode, iso, device) =>
-    ipcRenderer.invoke("flash-iso", mode, iso, device),
+    ipcRenderer.invoke(
+      "flash-iso",
+      mode,
+      iso,
+      device
+    ),
 
   selectIso: () =>
-    ipcRenderer.invoke("select-iso"),
+    ipcRenderer.invoke(
+      "select-iso"
+    ),
 
   getUsbDevices: () =>
-    ipcRenderer.invoke("get-usb-devices"),
+    ipcRenderer.invoke(
+      "get-usb-devices"
+    ),
 
-  onFlashEvent: (cb) => {
-    const handler = (_, data) => cb(data)
+  onFlashEvent: (callback) => {
 
-    ipcRenderer.on("flash-event", handler)
-    listeners.add(handler)
+    const handler = (_, event) =>
+      callback(event)
 
-    return () => {
-      ipcRenderer.removeListener("flash-event", handler)
-      listeners.delete(handler)
-    }
+    ipcRenderer.on(
+      "flash-event",
+      handler
+    )
+
+    return () =>
+      ipcRenderer.removeListener(
+        "flash-event",
+        handler
+      )
   },
 
-  cancelFlash: () => ipcRenderer.send("cancel-flash"),
-  pauseFlash: () => ipcRenderer.send("pause-flash"),
-  resumeFlash: () => ipcRenderer.send("resume-flash")
+  cancelFlash: () =>
+    ipcRenderer.send(
+      "cancel-flash"
+    )
 })
