9c06341ac1f5e00a17e78bb801d9429116915441
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-08 00:38:16 +0700

    Update retry_engine.cpp

--- backend-cpp/engine/retry_engine.cpp
+++ backend-cpp/engine/retry_engine.cpp
@@ -1,12 +1,18 @@
 #include <windows.h>
-#include "../disk/disk_write.cpp"
+#include <iostream>
+
+bool writeDisk(HANDLE h, BYTE* buffer, DWORD size) {
+    DWORD written = 0;
+
+    return WriteFile(h, buffer, size, &written, NULL);
+}
 
 bool retryWrite(HANDLE h, BYTE* buffer, DWORD size) {
     for (int i = 0; i < 3; i++) {
         if (writeDisk(h, buffer, size))
             return true;
 
-        Sleep(100);
+        Sleep(100 * (i + 1)); // backoff
     }
 
     return false;
