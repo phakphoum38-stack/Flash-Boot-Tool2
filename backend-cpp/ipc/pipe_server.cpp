ac441c7249288137c6c0226a912188196e371479
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-08 00:30:17 +0700

    Update pipe_server.cpp

--- backend-cpp/ipc/pipe_server.cpp
+++ backend-cpp/ipc/pipe_server.cpp
@@ -1,6 +1,21 @@
 #include <windows.h>
 #include <iostream>
 
-void startPipeServer() {
-    std::cout << "PIPE SERVER READY\n";
+void sendPipe(const std::string& msg) {
+    HANDLE pipe = CreateFileA(
+        R"(\\.\pipe\flash_engine)",
+        GENERIC_WRITE,
+        0,
+        NULL,
+        OPEN_EXISTING,
+        0,
+        NULL
+    );
+
+    if (pipe == INVALID_HANDLE_VALUE) return;
+
+    DWORD written;
+    WriteFile(pipe, msg.c_str(), msg.size(), &written, NULL);
+
+    CloseHandle(pipe);
 }
