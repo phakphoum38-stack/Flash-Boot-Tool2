2a8874ca6fdd587891eef4db235bed91f769b7e6
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-11 01:36:53 +0700

    Update LogBox.jsx

--- frontend-c/src/components/LogBox.jsx
+++ frontend-c/src/components/LogBox.jsx
@@ -1,16 +1,23 @@
-export default function LogBox({ logs }) {
+export default function LogBox({
+  logs = []
+}) {
 
   return (
     <div className="panel">
 
       <h3>📜 Logs</h3>
 
-      <div style={{ height: 200, overflow: "auto" }}>
+      <div
+        style={{
+          height: 200,
+          overflow: "auto"
+        }}
+      >
         {logs.map((l, i) => (
           <div key={i}>{l}</div>
         ))}
       </div>
 
     </div>
-  )
+  );
 }
