71408e520f246dd41f2059fdbef7dc6d6849851b
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-11 01:35:53 +0700

    Update FlashPanel.jsx

--- frontend-c/src/components/FlashPanel.jsx
+++ frontend-c/src/components/FlashPanel.jsx
@@ -1,7 +1,7 @@
 export default function FlashPanel({
-  progress,
-  speed,
-  result,
+  progress = 0,
+  speed = 0,
+  result = null,
   start
 }) {
 
@@ -12,15 +12,22 @@ export default function FlashPanel({
         🚀 START FLASH
       </button>
 
-      <div>Progress: {progress.toFixed(1)}%</div>
-      <div>Speed: {speed} MB/s</div>
+      <div>
+        Progress: {Number(progress).toFixed(1)}%
+      </div>
+
+      <div>
+        Speed: {speed} MB/s
+      </div>
 
       {result !== null && (
         <h3>
-          {result ? "✅ SUCCESS" : "❌ FAILED"}
+          {result
+            ? "✅ SUCCESS"
+            : "❌ FAILED"}
         </h3>
       )}
 
     </div>
-  )
+  );
 }
