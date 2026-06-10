54a1db30cf9a7eadf473e53810418c31718602a4
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-11 01:36:23 +0700

    Update ProgressRing.jsx

--- frontend-c/src/components/ProgressRing.jsx
+++ frontend-c/src/components/ProgressRing.jsx
@@ -1,17 +1,21 @@
-export default function ProgressRing({ progress }) {
+export default function ProgressRing({
+  progress = 0
+}) {
 
   return (
     <div className="ring">
 
       <h3>Progress</h3>
 
-      <div style={{
-        fontSize: "40px",
-        color: "#00ffcc"
-      }}>
-        {progress.toFixed(1)}%
+      <div
+        style={{
+          fontSize: "40px",
+          color: "#00ffcc"
+        }}
+      >
+        {Number(progress).toFixed(1)}%
       </div>
 
     </div>
-  )
+  );
 }
