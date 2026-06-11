60e5f562b89a5c618be16833350fc5cc4c56d5ab
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-11 15:51:33 +0700

    Create smart_mode.cpp

---
+++ backend-cpp/engine/smart_mode.cpp
@@ -0,0 +1,15 @@
+#include "smart_mode.h"
+
+#include <iostream>
+
+int runSmart(
+    const std::string& iso,
+    const std::string& device
+)
+{
+    std::cout
+        << "SMART MODE"
+        << std::endl;
+
+    return 0;
+}
