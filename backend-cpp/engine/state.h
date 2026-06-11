6f5353e0197d6bbd449044d98edb250aef15894c
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-11 15:21:09 +0700

    Update state.h

--- backend-cpp/engine/state.h
+++ backend-cpp/engine/state.h
@@ -1,10 +1,4 @@
 #pragma once
-#include <string>
 
-struct FlashState {
-    unsigned long long offset = 0;
-    unsigned long long total = 0;
-    std::string iso;
-    std::string device;
-    bool verified = false;
-};
+void setState(int state);
+int getState();
