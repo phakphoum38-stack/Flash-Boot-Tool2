b9b7acf6bcddd3344325e200ffd0eb7df252ce8b
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-11 16:12:58 +0700

    Update progress.cpp

--- backend-cpp/engine/progress.cpp
+++ backend-cpp/engine/progress.cpp
@@ -1,5 +1,33 @@
+#include "progress.h"
+
 #include <iostream>
 
-void emitProgress(double mb) {
-    std::cout << "PROGRESS:" << mb << std::endl;
+void emitProgress(
+    double percent
+)
+{
+    std::cout
+        << "PROGRESS:"
+        << percent
+        << std::endl;
+}
+
+void emitSpeed(
+    double mbps
+)
+{
+    std::cout
+        << "SPEED:"
+        << mbps
+        << std::endl;
+}
+
+void emitLog(
+    const char* msg
+)
+{
+    std::cout
+        << "LOG:"
+        << msg
+        << std::endl;
 }
