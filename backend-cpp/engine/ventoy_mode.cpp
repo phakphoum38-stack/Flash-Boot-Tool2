a16bba9f4e7d255b37d055b5ff919af08ec70636
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-11 16:05:58 +0700

    Update ventoy_mode.cpp

--- backend-cpp/engine/ventoy_mode.cpp
+++ backend-cpp/engine/ventoy_mode.cpp
@@ -1,5 +1,6 @@
 #include "ventoy_mode.h"
 
+#include <cstdlib>
 #include <iostream>
 
 int runVentoy(
@@ -8,8 +9,15 @@ int runVentoy(
 )
 {
     std::cout
-        << "VENTOY MODE"
+        << "LOG:VENTOY_START"
         << std::endl;
+
+    std::string cmd =
+        "resources\\ventoy\\Ventoy2Disk_X64.exe";
+
+    system(
+        cmd.c_str()
+    );
 
     return 0;
 }
