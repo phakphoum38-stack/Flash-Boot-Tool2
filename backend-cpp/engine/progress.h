ebba52a01ca97737df822445b59f28a48e4f497c
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-11 16:59:26 +0700

    Update progress.h

--- backend-cpp/engine/progress.h
+++ backend-cpp/engine/progress.h
@@ -1,3 +1,19 @@
-double calculateSpeed(
-    unsigned long long bytes
+#pragma once
+
+void emitLog(const char* msg);
+
+void emitProgress(
+    double percent
 );
+
+void emitVerify(
+    double percent
+);
+
+void emitSpeed(
+    double mbps
+);
+
+void emitDone();
+
+void emitFailed();
