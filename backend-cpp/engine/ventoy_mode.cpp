53af9c55c113c800b11225f9978aee6ef2470040
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-11 15:53:03 +0700

    Create ventoy_mode.cpp

---
+++ backend-cpp/engine/ventoy_mode.cpp
@@ -0,0 +1,15 @@
+#include "ventoy_mode.h"
+
+#include <iostream>
+
+int runVentoy(
+    const std::string& iso,
+    const std::string& device
+)
+{
+    std::cout
+        << "VENTOY MODE"
+        << std::endl;
+
+    return 0;
+}
