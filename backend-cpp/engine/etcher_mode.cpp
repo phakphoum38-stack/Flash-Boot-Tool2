41d7d18813051dc2d68617fe245c3e728a99aa7e
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-11 15:52:16 +0700

    Create etcher_mode.cpp

---
+++ backend-cpp/engine/etcher_mode.cpp
@@ -0,0 +1,15 @@
+#include "etcher_mode.h"
+
+#include <iostream>
+
+int runEtcher(
+    const std::string& iso,
+    const std::string& device
+)
+{
+    std::cout
+        << "ETCHER MODE"
+        << std::endl;
+
+    return 0;
+}
