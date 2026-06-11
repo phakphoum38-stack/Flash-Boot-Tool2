efd6480b7d92bf93a5c59eccc00cf36d8517972a
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-11 16:03:59 +0700

    Update dd_mode.cpp

--- backend-cpp/engine/dd_mode.cpp
+++ backend-cpp/engine/dd_mode.cpp
@@ -1,5 +1,10 @@
 #include "dd_mode.h"
 
+#include "../disk/disk_open.h"
+#include "../disk/disk_write.h"
+
+#include <windows.h>
+#include <fstream>
 #include <iostream>
 
 int runDD(
@@ -8,17 +13,102 @@ int runDD(
 )
 {
     std::cout
-        << "DD MODE"
+        << "LOG:DD_START"
         << std::endl;
 
-    std::cout
-        << "ISO : "
-        << iso
-        << std::endl;
+    std::ifstream file(
+        iso,
+        std::ios::binary
+    );
+
+    if (!file)
+    {
+        std::cout
+            << "LOG:ISO_OPEN_FAIL"
+            << std::endl;
+
+        return 1;
+    }
+
+    HANDLE disk =
+        CreateFileA(
+            device.c_str(),
+            GENERIC_WRITE,
+            FILE_SHARE_READ |
+            FILE_SHARE_WRITE,
+            NULL,
+            OPEN_EXISTING,
+            0,
+            NULL
+        );
+
+    if (disk == INVALID_HANDLE_VALUE)
+    {
+        std::cout
+            << "LOG:DISK_OPEN_FAIL"
+            << std::endl;
+
+        return 1;
+    }
+
+    const DWORD CHUNK =
+        4 * 1024 * 1024;
+
+    BYTE* buffer =
+        new BYTE[CHUNK];
+
+    unsigned long long total = 0;
+
+    while (file)
+    {
+        file.read(
+            (char*)buffer,
+            CHUNK
+        );
+
+        DWORD bytes =
+            (DWORD)file.gcount();
+
+        if (bytes == 0)
+            break;
+
+        DWORD written = 0;
+
+        if (!WriteFile(
+            disk,
+            buffer,
+            bytes,
+            &written,
+            NULL
+        ))
+        {
+            delete[] buffer;
+
+            CloseHandle(disk);
+
+            std::cout
+                << "LOG:WRITE_FAIL"
+                << std::endl;
+
+            return 1;
+        }
+
+        total += written;
+
+        std::cout
+            << "PROGRESS:"
+            << total
+            << std::endl;
+    }
+
+    delete[] buffer;
+
+    FlushFileBuffers(disk);
+
+    CloseHandle(disk);
 
     std::cout
-        << "DEVICE : "
-        << device
+        << "LOG:DD_DONE"
         << std::endl;
 
     return 0;
