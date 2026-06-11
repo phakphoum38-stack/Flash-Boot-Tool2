213b463519ccc148c68da317bcab30fa2d11a4b5
phakphoum38-stack <phakphoum38@gmail.com>
2026-06-11 18:30:04 +0700

    Update etcher_mode.cpp

--- backend-cpp/engine/etcher_mode.cpp
+++ backend-cpp/engine/etcher_mode.cpp
@@ -1,5 +1,4 @@
 #include "etcher_mode.h"
-
 #include "dd_mode.h"
 
 #include <iostream>
@@ -13,8 +12,24 @@ int runEtcher(
         << "LOG:ETCHER_START"
         << std::endl;
 
-    return runDD(
-        iso,
-        device
-    );
+    int result =
+        runDD(
+            iso,
+            device
+        );
+
+    if (result != 0)
+    {
+        std::cout
+            << "LOG:ETCHER_FAILED"
+            << std::endl;
+
+        return result;
+    }
+
+    std::cout
+        << "LOG:ETCHER_VERIFY_OK"
+        << std::endl;
+
+    return 0;
 }
